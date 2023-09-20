7287468ca2cc9ac138e514357e4a20c1f369caf7
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-20 17:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     // Get the values of the First and Last name
-    const firstName = document.getElementsByName("firstname")[0].value;
-    const lastName = document.getElementsByName("lastname")[0].value;
+    const firstName = document.getElementsByName("ftname")[0].value;
+    const lastName = document.getElementsByName("ltname")[0].value;
 
     // Display the values in an alert
     alert("First Name: " + firstName + "\nLast Name: " + lastName);
