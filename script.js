57efa444b1d2afe8c7ce857c2e44b8a88210d1f8
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-25 10:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function getFormvalue() {
-    // Get the form element by its ID
-    var form1 = document.getElementById('form1'');
+function getFormValue() {
+    // Get the form element
+    var form = document.getElementById('form1');
 
-    // Get the values of the First and Last name input fields
-    var fname = form.elements['fname'].value;
-    var lname = form.elements['lname'].value;
+    // Get the values of the first and last name input fields
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
 
     // Display the values in an alert
     alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
+
+    // Prevent the form from actually submitting
+    return false;
 }
